201998faa25fb36bd67c21a3c315bba04f104051
moise10r <moiserushanika2006@gmail.com>
2021-02-27 21:15:18 +0200

    first commit

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Aside from './aside';
+class Main extends Component {
+    // state = {  }
+    render() { 
+        return ( <div className="main">
+            <Aside/>
+        </div> );
+    }
+}
+ 
+export default Main;
